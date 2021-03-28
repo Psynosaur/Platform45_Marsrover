3fcf129de76528ad09c750d8a6030bfebb92e667
psynosaur@gmail.com <psynosaur@gmail.com>
2021-03-28 11:48:41 +0200

    Initial and final commit for platform 45 mars rover test

---
+++ Program.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Plafrorm45_MarsRover
+{
+    internal class Program
+    {
+        private static readonly string TestMessage =
+            "5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM\n0 0 E\nMLMMMMRMRMMLMM\n5 5 W\nMMMMMLMLMMMMMMMRMRMMMMMM";
+
+        // Our grid/plateau dimensions
+        private static int _lenX;
+
+        private static int _lenY;
+
+        // List of parked rovers
+        private static readonly List<Rover> ParkedRovers = new List<Rover>();
+
+        private static int GetHeadingDegrees(string heading)
+        {
+            return heading switch
+            {
+                "N" => 0,
+                "E" => 90,
+                "S" => 180,
+                "W" => 270,
+                _ => 0
+            };
+        }
+
+        private static string GetCardinalHeading(int heading)
+        {
+            return heading switch
+            {
+                0 => "N",
+                90 => "E",
+                180 => "S",
+                270 => "W",
+                _ => "N"
+            };
+        }
+
+        private static int[,] InitGrid(int upperRightX, int upperRightY)
+        {
+            // we add one to each coords since they start at 0,0
+            var grid = new int[upperRightX + 1, upperRightY + 1];
+            _lenX = grid.GetLength(0);
+            _lenY = grid.GetLength(1);
+            return grid;
+        }
+
+        private static bool CheckRoverInitOrMovePosition(int x, int y)
+        {
+            return x >= 0 && x < _lenX && y >= 0 && y < _lenY;
+        }
+
+        private static bool CheckRoverMoveForCollision(int x, int y, IEnumerable<Rover> rovers)
+        {
+            var checkCollision = rovers.Any(o => o.X == x && o.Y == y);
+            return checkCollision;
+        }
+
+
+        // Set the number at coordinate in the plateau to the rover number that explored it first
+        private static void MoveRover(Rover r, int heading, int[,] plateau)
+        {
+            // We can check here that the move would not result in the rover being out of bounds
+            // For all intends and purposes, Im going to discard a move instruction either if it
+            // would result in the rover being out of bounds or it would crash into another rover
+            switch (heading)
+            {
+                case 0:
+                    if (CheckRoverInitOrMovePosition(r.X, r.Y + 1))
+                    {
+                        if (CheckRoverMoveForCollision(r.X, r.Y + 1, ParkedRovers))
+                        {
+                            r.Errors++;
+                            var collider = ParkedRovers.FirstOrDefault(o => o.X == r.X && o.Y == r.Y + 1);
+                            if (collider != null)
+                                Console.WriteLine(
+                                    $"[ERROR] : Collision detected with rover {collider.Number}!!! skipping '{GetCardinalHeading(r.H)}' move command");
+                        }
+                        else
+                        {
+                            r.Y += 1;
+                        }
+
+                        // Claim the discovery for this coordinates
+                        if (plateau[r.X, r.Y] == 0) plateau[r.X, r.Y] = r.Number;
+                    }
+                    else
+                    {
+                        r.Errors++;
+                        Console.WriteLine(
+                            $"[ERROR] : Moving rover {r.Number} at x: {r.X} y: {r.Y} with heading '{GetCardinalHeading(r.H)}' would place " +
+                            $"it out of bounds!!!  skipping '{GetCardinalHeading(r.H)}' move command");
+                    }
+
+                    break;
+                case 90:
+                    if (CheckRoverInitOrMovePosition(r.X + 1, r.Y))
+                    {
+                        if (CheckRoverMoveForCollision(r.X + 1, r.Y, ParkedRovers))
+                        {
+                            r.Errors++;
+                            var collider = ParkedRovers.FirstOrDefault(o => o.X == r.X + 1 && o.Y == r.Y);
+                            if (collider != null)
+                                Console.WriteLine(
+                                    $"[ERROR] : Collision detected with rover {collider.Number}!!! skipping '{GetCardinalHeading(r.H)}' move command");
+                        }
+                        else
+                        {
+                            r.X += 1;
+                        }
+
+                        // Claim the discovery for this coordinates
+                        if (plateau[r.X, r.Y] == 0) plateau[r.X, r.Y] = r.Number;
+                    }
+                    else
+                    {
+                        r.Errors++;
+                        Console.WriteLine(
+                            $"[ERROR] : Moving rover {r.Number} at x: {r.X} y: {r.Y} with heading '{GetCardinalHeading(r.H)}' would place " +
+                            $"it out of bounds!!!  skipping '{GetCardinalHeading(r.H)}' move command");
+                    }
+
+                    break;
+                case 180:
+                    if (CheckRoverInitOrMovePosition(r.X, r.Y - 1))
+                    {
+                        if (CheckRoverMoveForCollision(r.X, r.Y - 1, ParkedRovers))
+                        {
+                            r.Errors++;
+                            var collider = ParkedRovers.FirstOrDefault(o => o.X == r.X && o.Y == r.Y - 1);
+                            if (collider != null)
+                                Console.WriteLine(
+                                    $"[ERROR] : Collision detected with rover {collider.Number}!!! skipping '{GetCardinalHeading(r.H)}' move command");
+                        }
+                        else
+                        {
+                            r.Y -= 1;
+                        }
+
+                        // Claim the discovery for this coordinates
+                        if (plateau[r.X, r.Y] == 0) plateau[r.X, r.Y] = r.Number;
+                    }
+                    else
+                    {
+                        r.Errors++;
+                        Console.WriteLine(
+                            $"[ERROR] : Moving rover {r.Number} at x: {r.X} y: {r.Y} with heading '{GetCardinalHeading(r.H)}' would place " +
+                            $"it out of bounds!!!  skipping '{GetCardinalHeading(r.H)}' move command");
+                    }
+
+                    break;
+                case 270:
+                    if (CheckRoverInitOrMovePosition(r.X - 1, r.Y))
+                    {
+                        if (CheckRoverMoveForCollision(r.X - 1, r.Y, ParkedRovers))
+                        {
+                            r.Errors++;
+                            var collider = ParkedRovers.FirstOrDefault(o => o.X == r.X - 1 && o.Y == r.Y);
+                            if (collider != null)
+                                Console.WriteLine(
+                                    $"[ERROR] : Collision detected with rover {collider.Number}!!! skipping '{GetCardinalHeading(r.H)}' move command");
+                        }
+                        else
+                        {
+                            r.X -= 1;
+                        }
+
+                        // Claim the discovery for this coordinates
+                        if (plateau[r.X, r.Y] == 0) plateau[r.X, r.Y] = r.Number;
+                    }
+                    else
+                    {
+                        r.Errors++;
+                        Console.WriteLine(
+                            $"[ERROR] : Moving rover {r.Number} at x: {r.X} y: {r.Y} with heading '{GetCardinalHeading(r.H)}' would place " +
+                            $"it out of bounds!!!  skipping '{GetCardinalHeading(r.H)}' move command");
+                    }
+
+                    break;
+            }
+        }
+
+        private static void TurnRover(Rover r, char turn)
+        {
+            if (turn == 'R')
+            {
+                r.H += 90;
+                // when h is 360°, we set it to 0° so that we get 90°(E) when adding 90° next time
+                if (r.H == 360) r.H = 0;
+            }
+            else
+            {
+                // when h is 0°, we set it to 360° so that we get 270°(W) when subtracting 90°
+                if (r.H == 0) r.H = 360;
+                r.H -= 90;
+            }
+        }
+
+        private static void Main()
+        {
+            try
+            {
+                // split our TestMessage into a 'stack' of strings
+                var cmdList = TestMessage.Split('\n').ToList();
+
+                // line one of the cmdList 'stack' is the upper coordinates of our plateau
+                string[] upperCoords;
+                upperCoords = cmdList.FirstOrDefault()?.Split(' ');
+
+                // remove line one of the cmdList 'stack' since we've used it values
+                cmdList = cmdList.Skip(1).ToList();
+
+                // setup our plateau / matrix
+                int[,] plateau = { };
+                if (upperCoords != null)
+                    plateau = InitGrid(
+                        Convert.ToInt32(upperCoords[0]),
+                        Convert.ToInt32(upperCoords[1]));
+
+                // cmdList minus first line(5 5) used for plateau setup divided by 2 lines per rover 
+                // >>> 1st line is rover 'start state'
+                // >>> 2nd line is rover 'do stuff'
+                var roverCount = cmdList.Count / 2;
+
+                Console.WriteLine($"The test input has {roverCount} {(roverCount > 1 ? "rovers" : "rover")}");
+                // Process rover commands synchronously per rover
+                for (var i = 1; i <= roverCount; i++)
+                {
+                    // first line is used to initialize the rover with a starting position
+                    var initCommandLine = cmdList.FirstOrDefault()?.Split(" ");
+
+                    // remove the item from cmdList 'stack' since we've used it values
+                    cmdList = cmdList.Skip(1).ToList();
+
+                    // Init a rover class
+                    if (initCommandLine != null)
+                    {
+                        var initX = Convert.ToInt32(initCommandLine[0]);
+                        var initY = Convert.ToInt32(initCommandLine[1]);
+                        var heading = GetHeadingDegrees(initCommandLine[2]);
+                        Rover rover;
+                        if (CheckRoverInitOrMovePosition(initX, initY))
+                        {
+                            rover = new Rover(initX, initY, heading);
+                            // set the start position as explored too
+                            plateau[rover.X, rover.Y] = i;
+                            Console.WriteLine($"\nDeploying rover {i} on validated coordinates" +
+                                              $" {rover.X} {rover.Y} {GetCardinalHeading(rover.H)} ");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Rover {i} out of bounds, trying to deploy the next one");
+                            // remove the commandLine from the cmdList 'stack' since we've used it and it was invalid
+                            cmdList = cmdList.Skip(1).ToList();
+                            // and move on to the next rover without doing stuff
+                            continue;
+                        }
+
+                        // after removing the rover init line from the cmdList 'stack', we have the "do stuff" line
+                        // for this initialized rover
+                        var doStuffCommandLine = cmdList.FirstOrDefault()?.ToCharArray();
+                        if (doStuffCommandLine != null)
+                        {
+                            rover.CmdCount = doStuffCommandLine.Length;
+                            rover.Number = i;
+                            foreach (var c in doStuffCommandLine)
+                                if (c == 'L' || c == 'R')
+                                    TurnRover(rover, c);
+                                else MoveRover(rover, rover.H, plateau);
+                        }
+
+                        // remove the commandLine from the cmdList 'stack' since we've used it
+                        cmdList = cmdList.Skip(1).ToList();
+                        Console.WriteLine($"The state of the plateau after rover {i} finished its commands\n");
+
+                        // Use this for collision detection
+                        ParkedRovers.Add(rover);
+                    }
+
+                    // Print the rovers progress
+                    for (var x = plateau.GetLength(1) - 1; x >= 0; x--)
+                    {
+                        for (var y = 0; y < plateau.GetLength(0); y++) Console.Write(plateau[y, x] + " ");
+                        Console.WriteLine();
+                    }
+                }
+
+                // Let NASA know how many pixels are unexplored within our plateau
+                var cnt = plateau.Cast<int>().Count(num => num == 0);
+                Console.WriteLine($"\nUnexplored pixels in our plateau {cnt}");
+                // Summarize rover states, again
+                var index = 1;
+                foreach (var rover in ParkedRovers)
+                {
+                    Console.WriteLine(
+                        $"\nRover {index} \nOutput: {rover.X} {rover.Y} {GetCardinalHeading(rover.H)}" +
+                        $"\nProcessed : {rover.CmdCount} commands with {(rover.Errors > 0 ? $"{rover.Errors} errors" : "no errors")} ");
+                    index++;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private class Rover
+        {
+            public Rover(int x, int y, int h)
+            {
+                X = x;
+                Y = y;
+                H = h;
+            }
+
+            public int Number { get; internal set; }
+            public int X { get; internal set; }
+            public int Y { get; internal set; }
+            public int H { get; internal set; }
+            public int CmdCount { get; set; }
+            public int Errors { get; set; }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,145 @@
+## Platform45 - Mars Rover challenge
+
+### These are my observations and notes, they are not in any particulare order, however the code is structured in such a way that these observations make more sense.
+
+
+These are my assumptions and notes on the problem which I will turn into code.
+- This plateau, which is curiously rectangular, must be navigated by the rovers so that their on board cameras can get a complete view of the surrounding terrain to send back to Earth.
+- A rover's position is represented by a combination of an x and y co-ordinates and a letter representing one of the four cardinal compass points.
+- The plateau is divided up into a grid to simplify navigation.
+    - An example position might be 0, 0, N, which means the rover is in the bottom left corner and facing North.
+- In order to control a rover, NASA sends a simple string of letters.
+    - The possible letters are 'L', 'R' and 'M'.
+    - This defines "turning" the rover
+        - 'L' and 'R' makes the rover spin 90 degrees left or right respectively, without moving from its current spot.
+    - This defines "moving" the rover
+        - 'M' means move forward one grid point, and maintain the same heading.
+- Assume that the square/coordinate directly North from (x, y) is (x, y+1).
+    - THIS IS IMPORTANT !!
+    - It defines what a rover does when moving up N
+        - Northern movement : (x, y+1)
+        - From this we can discern the following
+            - Eastern movement : (x+1, y)
+            - Southern movement : (x,y -1)
+            - Western movement : (x-1,y)
+        - We should use this is a case statement to change coordinates based on the current direction of the rover(s)
+
+### Input:
+- The first line of input is the upper-right coordinates of the plateau, the lower-left coordinates are assumed to be 0,0.
+- Assumptions :
+    - Therefor the range from 0 - 5 is 6 units of change
+    - Our array(plateau) size can then calculated with upper right x an y coords namely urx and ury
+    - lenX = 0
+    - lenY = 0
+        - For x in range(0, urx):
+            - lenX += 1
+        - For y in range(0, ury):
+            - lenY += 1
+    - arraySize = lenX * lenY
+
+- The rest of the input is information pertaining to the rovers that have been deployed.
+    - Each rover has two lines of input.
+    - The first line gives the rover's position,
+    - The second line is a series of instructions telling the rover how to explore the plateau.
+
+- The position is made up of two integers and a letter separated by spaces, corresponding to the x and y co-ordinates and the rover's orientation.
+
+###Each rover will be finished sequentially, which means that the second rover won't start to move until the first one has finished moving.
+
+### Output:
+
+The output for each rover should be its final co-ordinates and heading.
+
+### Test Input:
+#### Plateau size is a 6x6 matrix given the start coordinates of 0,0
+- 5 5
+  
+####Rover 1 - Message
+- Position / start - Line 1
+    - 1 2 N
+- Instruction - Line 2
+    - LMLMLMLMM
+      
+####Rover 2 - Message
+- Position / start - Line 1
+    - 3 3 E
+- Instruction - Line 2
+    - MMRMMRMRRM
+
+#### Expected Output:
+
+1 3 N
+
+5 1 E
+
+####Create function for cardinal directions: 
+
+    N = 0° / 360°
+    E = 90°
+    S = 180°
+    W = 270°
+
+####Define a rover state: I never used this :D
+
+    setState(rover)
+        // state object, only changes one attribute at a time
+        state = {
+               x : rover.x,
+               y : rover.y,
+               h : rover.h
+        }
+
+####Define turning as a function:
+        
+        // NOTE --> 360° = 0° 
+        turnRover(h, turn):
+            If turn R:
+                h = h + 90
+                // when h is 360°, we set it to 0° so that we get 90°(E) when adding 90°
+                if h == 360:
+                    h = 0
+            If turn L:
+                // when h is 0°, we set it to 360° so that we get 270°(W) when subtracting 90°
+                if h == 0:
+                    h = 360
+                h = h - 90
+####Define moving as a function: 
+        
+        moveRover(currentX, currentY, h):
+            // integer array for our coordinates
+            Switch h:
+                Case N:
+                    state = [currentX, currentY+1, h)
+                Case E:
+                    state = [currentX+1, currentY, h)
+                Case S:
+                    state = (currentX, currentY-1, h)
+                Case W:
+                    state = [currentX-1, currentY, h)
+
+### Interpret the instructions
+- R = + 90° to current direction
+- L = - 90° to current direction
+- M = increase position in current heading by 1
+### Assumptions and observations
+- Turning does not change coordinates only the rovers heading
+- Moving changes rovers coordinates but not heading
+- Can they go out of bounds?
+    - We should not expect NASA to have rovers that go out of bounds
+- If they collide?
+    - do they stop?
+    - explode?
+    - Engineering team at NASA decided just to skip commands that where erroneous
+### Ways to solve problem :
+1. create a loop that executes the following commands per rover for n amounts of rovers
+    - Init the rover if the init coordinates are valid
+
+2. create another loop within that splits the byes of the command into separate instructions characters
+            
+        foreach char in command:
+            if char L or R:
+                turnRover(h, char)
+            if char M:
+                moveRover(x, y, h)
+    - change the state of the current rover based on the type(turning/moving) of instruction byte
+    - Output the coordinates and heading of the current rover
